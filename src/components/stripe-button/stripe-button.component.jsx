f62817bfc9bcac6007a725d5ff0707cf394cdca4
陳鎮宇 <ccking37@gmail.com>
2020-04-07 18:30:41 +0800

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+	const priceForStripe = price * 100;
+	const publishableKey = 'pk_test_VA5Tmo06X25Edt37wMptffcV00f6XxskZL';
+
+	const onToken = token => {
+		console.log(token);
+		alert('Payment Successful');
+	}
+
+	return (
+		<StripeCheckout
+			label='Pay Now'
+			name='CRWN Clothing Ltd.'
+			billingAddress
+			shippingAddress
+			image='https://svgshare.com/i/CUz.svg'
+			description={`Your total is $${price}`}
+			amount={priceForStripe}
+			panelLabel='Pay Now'
+			token={onToken}
+			stripeKey={publishableKey}
+		/>
+	);
+};
+
+export default StripeCheckoutButton;
